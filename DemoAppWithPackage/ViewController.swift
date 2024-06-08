a812922ce6e5ad2a6a8cc6cb0d1b4bf14dbe6277
Vishal K <vishalkamble8939@gmail.com>
2024-06-08 15:08:53 +0530

    integration UserRequestKit package

--- DemoAppWithPackage/ViewController.swift
+++ DemoAppWithPackage/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import UserRequestKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func clickToCallPackageBtn(_ sender: UIButton) {
+        UserRequestKit.presentAsTopViewController { userEmail in
+                if let email = userEmail {
+                    self.emailLbl.text = email
+                } else {
+                    print("No user email returned")
+                }
+            }
+    }
 
 }
 
